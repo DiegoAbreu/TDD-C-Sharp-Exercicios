074a1aab4b88939771bfa0c5418ef9dd3ef00e1a
DiegoAbreu <diego.souza.abreu@gmail.com>
2017-03-07 00:11:34 -0300

    TDD C-Sharp - NUnit -Exercícios alura

---
+++ Teste de Unidade/Caelum.Leilao/Caelum.Leilao/LeilaoTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class LeilaoTest
+    {
+        [Test] //leilao com um lance
+        public void DeveReceberUmLance()
+        {
+            Leilao leilao = new Leilao("MacBook Pro 15");
+            Assert.AreEqual(0, leilao.Lances.Count);
+
+            var usuario = new Usuario("Steve Jobs");
+            leilao.Propoe(new Lance(usuario, 2000));
+
+            Assert.AreEqual(1, leilao.Lances.Count);
+            Assert.AreEqual(2000, leilao.Lances[0].Valor, 0.00001);
+        }
+        [Test] //Deve com varios lances
+        public void DeveReceberVariosLances()
+        {
+            Leilao leilao = new Leilao("MacBook Pro 15");
+            Assert.AreEqual(0, leilao.Lances.Count);
+
+            var usuario = new Usuario("Steve Jobs");
+            var usuario1 = new Usuario("Steve Wozniac");
+            leilao.Propoe(new Lance(usuario, 2000));
+            leilao.Propoe(new Lance(usuario1, 3000));
+
+            Assert.AreEqual(2, leilao.Lances.Count);
+            Assert.AreEqual(2000, leilao.Lances[0].Valor, 0.00001);
+            Assert.AreEqual(3000, leilao.Lances[1].Valor, 0.00001);
+        }
+        [Test] //Nao Deve aceitar lances seguidos de um mesmo usuario
+        public void NaoDeveAceitarDoisLancesSeguidosDoMesmoUsuario()
+        {
+            Leilao leilao = new Leilao("MacBook Pro 15");
+            Assert.AreEqual(0, leilao.Lances.Count);
+
+            var usuario = new Usuario("Steve Jobs");
+            var usuario1 = new Usuario("Steve Wozniac");
+            leilao.Propoe(new Lance(usuario, 2000));
+            leilao.Propoe(new Lance(usuario, 2500));
+            leilao.Propoe(new Lance(usuario1, 3000));
+
+            Assert.AreEqual(2, leilao.Lances.Count);
+            Assert.AreEqual(2000, leilao.Lances[0].Valor, 0.00001);
+            Assert.AreEqual(3000, leilao.Lances[1].Valor, 0.00001);
+        }
+
+        [Test] //Nao Deve aceitar mais que 5 lances de um mesmo usuario
+        public void NaoDeveAceitarMaisDoQueCincoLancesDeUmMesmoUsuario()
+        {
+            Leilao leilao = new Leilao("MacBook Pro 15");
+            Assert.AreEqual(0, leilao.Lances.Count);
+
+            var usuario = new Usuario("Steve Jobs");
+            var usuario1 = new Usuario("Steve Wozniac");
+            leilao.Propoe(new Lance(usuario, 2000));
+            leilao.Propoe(new Lance(usuario1, 3000));
+
+            leilao.Propoe(new Lance(usuario, 4000));
+            leilao.Propoe(new Lance(usuario1, 5000));
+
+            leilao.Propoe(new Lance(usuario, 5500));
+            leilao.Propoe(new Lance(usuario1, 5600));
+
+            leilao.Propoe(new Lance(usuario, 6000));
+            leilao.Propoe(new Lance(usuario1, 6200));
+
+            leilao.Propoe(new Lance(usuario, 6500));
+            leilao.Propoe(new Lance(usuario1, 6800));
+
+            //Esses lances devem ser ignorados
+            leilao.Propoe(new Lance(usuario, 7000));
+            leilao.Propoe(new Lance(usuario1, 7500));
+
+            Assert.AreEqual(10, leilao.Lances.Count);
+            var ultimo = leilao.Lances.Count - 1;
+            Lance ultimoLance = leilao.Lances[ultimo];
+
+            Assert.AreEqual(6800, ultimoLance.Valor, 0.00001);
+         
+        }
+    }
+}
+  
---
+++ Teste de Unidade/Caelum.Leilao/Caelum.Leilao/TesteDoAvaliador.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.VisualStudio.TestAdapter;
+using NuGet.VisualStudio;
+
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class TesteDoAvaliador
+    {
+        private Avaliador leiloeiro;
+        private Usuario joao;
+        private Usuario jose;
+        private Usuario maria;
+
+        [SetUp]
+        public void criaAvaliador()
+        {
+            this.leiloeiro = new Avaliador();
+            this.joao = new Usuario("Joao");
+            this.jose = new Usuario("Jose");
+            this.maria = new Usuario("Maria");
+        }
+
+        [Test] //teste 1: Lances em ordem crescente
+        public void DeveEntenderLancesEmOrdemCrescente()
+        {
+            //1a parte: cenario
+            Leilao leilao = new CriadorDeLeilao().Para("Playstation 4 Novo")
+            .Lance(maria, 250.0)
+            .Lance(joao, 300.0)
+            .Lance(jose, 400.0)
+            .Constroi();
+
+            //2a parte: acao
+            leiloeiro.Avalia(leilao);
+
+            //3a parte: validacao
+            double maiorEsperado = 400;
+            double menorEsperado = 250;
+
+            Assert.AreEqual(maiorEsperado, leiloeiro.MaiorLance);
+            Assert.AreEqual(menorEsperado, leiloeiro.MenorLance);
+
+        }
+
+        [Test] //teste 2: Lances em ordem crescente com outros valores
+        public void DeveEntenderLancesEmOrdemCrescenteComOutrosValores()
+        {
+            //1a parte: cenario
+            Leilao leilao = new CriadorDeLeilao().Para("Playstation 4 Novo")
+            .Lance(maria, 1000.0)
+            .Lance(joao, 2000.0)
+            .Lance(jose, 3000.0)
+            .Constroi();
+
+            //2a parte: acao
+            leiloeiro.Avalia(leilao);
+
+            //3a parte: validacao
+            Assert.AreEqual(3000, leiloeiro.MaiorLance);
+            Assert.AreEqual(1000, leiloeiro.MenorLance);
+
+        }
+        [Test] //teste 3: Leilao com um unico lance
+        public void DeveEntenderLeilaoComApenasUmLance()
+        {
+            //1a parte: cenario
+            Leilao leilao = new CriadorDeLeilao().Para("Playstation 4 Novo")
+                .Lance(joao, 1000.0)
+                .Constroi();
+
+            //2a parte: acao
+            leiloeiro.Avalia(leilao);
+
+            //3a parte: validacao
+            Assert.AreEqual(1000, leiloeiro.MaiorLance, 0.0001);
+            Assert.AreEqual(1000, leiloeiro.MenorLance, 0.0001);
+
+        }
+        [Test] //teste 4: Lances em ordem Decrescente
+        public void DeveEntenderLancesEmOrdemDecrescente()
+        {
+            //1a parte: cenario
+            Leilao leilao = new CriadorDeLeilao().Para("Playstation 4 Novo")
+            .Lance(joao, 3000.0)
+            .Lance(maria, 2000.0)
+            .Lance(jose, 1000.0)
+            .Constroi();
+
+            //2a parte: acao
+            leiloeiro.Avalia(leilao);
+
+            //3a parte: validacao
+            Assert.AreEqual(3000, leiloeiro.MaiorLance);
+            Assert.AreEqual(1000, leiloeiro.MenorLance);
+        }
+        [Test] //teste 5: Lances em ordem Randomica
+        public void DeveEntenderLancesEmOrdemRandomica()
+        {
+            //1a parte: cenario
+            Leilao leilao = new CriadorDeLeilao().Para("Playstation 4 Novo")
+                .Lance(joao, 500.0)
+                .Lance(maria, 7000.0)
+                .Lance(jose, 1000.0)
+                .Constroi();
+
+            //2a parte: acao
+            leiloeiro.Avalia(leilao);
+
+            //3a parte: validacao
+            Assert.AreEqual(7000, leiloeiro.MaiorLance);
+            Assert.AreEqual(500, leiloeiro.MenorLance);
+        }
+        [Test] //teste 6: Encontra os tres maiores lances
+        public void DeveEncontrarOsTresMaioresLances()
+        {
+            //1a parte: cenario
+            Leilao leilao = new CriadorDeLeilao().Para("Playstation 4 Novo")
+                .Lance(joao, 500.0)
+                .Lance(maria, 600.0)
+                .Lance(joao, 800.0)
+                .Lance(maria, 1200.0)
+                .Lance(joao, 1500.0)
+                .Lance(maria, 1700.0)
+                .Constroi();
+
+            //2a parte: acao
+            leiloeiro.Avalia(leilao);
+            var maiores = leiloeiro.TresMaiores;
+
+            //3a parte: validacao
+            Assert.AreEqual(3, maiores.Count);
+            Assert.AreEqual(1700.0, maiores[0].Valor, 0.0001);
+            Assert.AreEqual(1500.0, maiores[1].Valor, 0.0001);
+            Assert.AreEqual(1200.0, maiores[2].Valor, 0.0001);
+        }
+
+        [Test]//leilao sem lances
+        [ExpectedException( typeof( Exception))]
+        public void NaoDeveAvaliarLeilaoSemLances()
+        {
+                Leilao leilao = new CriadorDeLeilao().Para("Geladeira")
+                .Constroi();
+
+                leiloeiro.Avalia(leilao);
+        }
+    }
+}
+   
+
